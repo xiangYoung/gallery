601ff5ebbc975d4c7e11dc75f0da85f862dcd145
xiangYoung <xiangyoung@yeah.net>
2018-07-27 16:02:50 +0800

    finish

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,10 +17,55 @@ imageDatas = (function getImageDataURL(imageDataArr) {
   }
   return imageDataArr;
 })(imageDatas);
-
-class SingleFigure extends React.Component{
+// 图片组件
+class SingleFigure extends React.Component {
   // 点击处理函数
   handleClick = (e) => {
+    if (this.props.arrange.isCenter) {
+      this.props.inverse();
+    }
+    else {
+      this.props.center();
+    }
+    e.stopPropagation();
+    e.preventDefault();
+  }
+  render() {
+    let styleObj = {};
+
+    if (this.props.arrange.pos) {
+      styleObj = this.props.arrange.pos;
+    }
+    if (this.props.arrange.rotate) {
+      (['-moz-', '-ms-', '-webkit-', '']).forEach(function (value) {
+        styleObj[value + 'transform'] = 'rotate(' + this.props.arrange.rotate + 'deg)';
+      }.bind(this))
+
+    }
+    if (this.props.arrange.isCenter) {
+      styleObj['zIndex'] = 11;
+    }
+    let imgFigureClassName = 'imgFigure';
+    imgFigureClassName += this.props.arrange.isInverse ? ' isInverse' : '';
+    const { data } = this.props
+    return (
+      <figure className={imgFigureClassName}
+        style={styleObj}
+        onClick={this.handleClick}>
+        <img src={data.imageURL} alt={data.title} style={{ width: 240, height: 240 }} />
+        <figcaption>
+          <h2 className='imgTitle'>{data.title}</h2>
+          <div className='imgBack' onClick={this.handleClick}>
+            <p>{data.desc}</p>
+          </div>
+        </figcaption>
+      </figure>
+    )
+  }
+}
+// 控制组件
+class Controller extends React.Component {
+  handleClick = (e) =>  {
     if(this.props.arrange.isCenter){
       this.props.inverse();
     }
@@ -30,81 +75,62 @@ class SingleFigure extends React.Component{
     e.stopPropagation();
     e.preventDefault();
   }
-  render(){
-    let styleObj = {};
-
-    if(this.props.arrange.pos){
-      styleObj = this.props.arrange.pos;
-    }
-    if(this.props.arrange.rotate){
-      (['-moz-','-ms-','-webkit-','']).forEach(function(value){
-        styleObj[value + 'transform'] = 'rotate(' + this.props.arrange.rotate + 'deg)'; 
-      }.bind(this))
-      
-    }
+  render() {
+    let controllerUnitClassName = 'controllerUnit';
     if(this.props.arrange.isCenter){
-      styleObj['zIndex'] = 11; 
+      controllerUnitClassName += ' isControllCenter';
+      if(this.props.arrange.isInverse){ 
+        controllerUnitClassName += ' isControllInverse';
+      }
     }
-    let imgFigureClassName = 'imgFigure';
-    imgFigureClassName += this.props.arrange.isInverse ? ' isInverse' : '';
-    const { data } = this.props
     return (
-      <figure className={imgFigureClassName} 
-        style={styleObj}
-        onClick={this.handleClick}>
-        <img src={data.imageURL} alt={data.title} style={{ width: 240, height: 240}}/>
-        <figcaption>
-          <h2 className='imgTitle'>{data.title}</h2>
-          <div className='imgBack' onClick={this.handleClick}>
-          <p>{data.desc}</p>
-          </div>
-        </figcaption>
-      </figure>
+      <span className={controllerUnitClassName} onClick={this.handleClick}>
+      </span >
     )
   }
 }
 class AppComponent extends React.Component {
   state = {
-    Constant:{
-      centerPos : {
+    Constant: {
+      centerPos: {
         left: 0,
         top: 0
       },
       hPosRange: {
-        leftSecX: [0,0],
-        rightSecX: [0,0],
-        y: [0,0]
+        leftSecX: [0, 0],
+        rightSecX: [0, 0],
+        y: [0, 0]
       },
       vPosRange: {
-        x: [0,0],
-        topY: [0,0]
+        x: [0, 0],
+        topY: [0, 0]
       }
     },
-    imgArrangeArr : [
+    imgArrangeArr: [
       {
-        pos:{ 
-          left:0,
-          top:0
+        pos: {
+          left: 0,
+          top: 0
         },
-        rotate : 0,// 旋转
+        rotate: 0,// 旋转
         isInverse: false, // 正反面
         isCenter: false,// 是否剧中
       }
     ]
   }
-  componentDidMount(){
+  componentDidMount() {
     const { Constant } = this.state;
     let wrapDOM = ReactDOM.findDOMNode(this.refs.wrap),
-    stageW = wrapDOM.scrollWidth,
-    stageH = wrapDOM.scrollHeight,
-    halfStageW = Math.ceil(stageW / 2),
-    halfStageH = Math.ceil(stageH / 2);
+      stageW = wrapDOM.scrollWidth,
+      stageH = wrapDOM.scrollHeight,
+      halfStageW = Math.ceil(stageW / 2),
+      halfStageH = Math.ceil(stageH / 2);
 
     let imgFigureDOM = ReactDOM.findDOMNode(this.refs.imgFigure0),
-        imgW = imgFigureDOM.scrollWidth,
-        imgH = imgFigureDOM.scrollHeight,
-        halfImgW = Math.ceil(imgW / 2),
-        halfImgH = Math.ceil(imgH / 2);
+      imgW = imgFigureDOM.scrollWidth,
+      imgH = imgFigureDOM.scrollHeight,
+      halfImgW = Math.ceil(imgW / 2),
+      halfImgH = Math.ceil(imgH / 2);
 
     Constant.centerPos = {
       left: halfStageW - halfImgW,
@@ -118,7 +144,7 @@ class AppComponent extends React.Component {
     Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
     Constant.hPosRange.y[0] = -halfImgH;
     Constant.hPosRange.y[1] = stageH - halfImgH;
-    
+
     Constant.vPosRange.topY[0] = -halfImgH;
     Constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
     Constant.vPosRange.x[0] = halfStageW - imgW;
@@ -127,8 +153,8 @@ class AppComponent extends React.Component {
     this.rearrange(0);
   }
   // 翻转图片
-  inverse(index){
-    return function(){
+  inverse(index) {
+    return function () {
       let { imgArrangeArr } = this.state;
       imgArrangeArr[index].isInverse = !imgArrangeArr[index].isInverse;
 
@@ -138,69 +164,69 @@ class AppComponent extends React.Component {
     }.bind(this)
   }
   // 获取区间随机值
-  getRangeRandom(low,high){
+  getRangeRandom(low, high) {
     return Math.floor(Math.random() * (high - low) + low);
   }
   // 获取角度值
-  get30DegRandom(){
-    return (Math.random() > 0.5 ?  '' : '-') + Math.ceil(Math.random() * 30);
+  get30DegRandom() {
+    return (Math.random() > 0.5 ? '' : '-') + Math.ceil(Math.random() * 30);
   }
   // 重新布局
-  rearrange(centerIndex){
-    let { imgArrangeArr, Constant :{ centerPos,hPosRange:{leftSecX,rightSecX,y},vPosRange:{topY,x}} } = this.state;
+  rearrange(centerIndex) {
+    let { imgArrangeArr, Constant: { centerPos, hPosRange: { leftSecX, rightSecX, y }, vPosRange: { topY, x } } } = this.state;
 
     let imgsArrangeTopArr = [];
     let topImgNum = Math.floor(Math.random() * 2);//0 1
     let topImgSpliceIndex = 0;
 
-    let imgsArrangeCenterArr = imgArrangeArr.splice(centerIndex,1);
+    let imgsArrangeCenterArr = imgArrangeArr.splice(centerIndex, 1);
     // 布局中间
     imgsArrangeCenterArr[0] = {
-      pos : centerPos,
-      rotate : 0,
+      pos: centerPos,
+      rotate: 0,
       isCenter: true
     };
 
     topImgSpliceIndex = Math.floor(Math.random() * (imgArrangeArr.length - topImgNum));
-    imgsArrangeTopArr = imgArrangeArr.splice(topImgSpliceIndex,topImgNum);
-    
+    imgsArrangeTopArr = imgArrangeArr.splice(topImgSpliceIndex, topImgNum);
+
     // 布局于上侧
-    imgsArrangeTopArr.forEach( (value,index) => {
+    imgsArrangeTopArr.forEach((value, index) => {
       imgsArrangeTopArr[index] = {
-        pos : {
-          top: this.getRangeRandom(topY[0],topY[1]),
-          left: this.getRangeRandom(x[0],x[1])
+        pos: {
+          top: this.getRangeRandom(topY[0], topY[1]),
+          left: this.getRangeRandom(x[0], x[1])
         },
-        rotate : this.get30DegRandom(),
+        rotate: this.get30DegRandom(),
         isCenter: false
       }
     })
 
     // 布局左右两侧
-    for(let i = 0,j = imgArrangeArr.length, k = j / 2; i < j; i++){
+    for (let i = 0, j = imgArrangeArr.length, k = j / 2; i < j; i++) {
       let hposRangeLOX = null;
-      if(i < k){
+      if (i < k) {
         hposRangeLOX = leftSecX;
       }
-      else{
+      else {
         hposRangeLOX = rightSecX;
       }
 
       imgArrangeArr[i] = {
-        pos:{
-          top: this.getRangeRandom(y[0],y[1]),
-          left: this.getRangeRandom(hposRangeLOX[0],hposRangeLOX[1])
+        pos: {
+          top: this.getRangeRandom(y[0], y[1]),
+          left: this.getRangeRandom(hposRangeLOX[0], hposRangeLOX[1])
         },
-        rotate : this.get30DegRandom(),
+        rotate: this.get30DegRandom(),
         isCenter: false
       }
     }
 
-    if(imgsArrangeTopArr && imgsArrangeTopArr[0]){
-      imgArrangeArr.splice(topImgSpliceIndex,0,imgsArrangeTopArr[0]);
+    if (imgsArrangeTopArr && imgsArrangeTopArr[0]) {
+      imgArrangeArr.splice(topImgSpliceIndex, 0, imgsArrangeTopArr[0]);
     }
 
-    imgArrangeArr.splice(centerIndex,0,imgsArrangeCenterArr[0]);
+    imgArrangeArr.splice(centerIndex, 0, imgsArrangeCenterArr[0]);
 
     this.setState({
       imgArrangeArr: imgArrangeArr
@@ -208,32 +234,28 @@ class AppComponent extends React.Component {
 
   }
   // 利用上面函数 剧中对应index的图片
-  center(index){
-    return function(){
+  center(index) {
+    return function () {
       this.rearrange(index);
     }.bind(this)
   }
   render() {
     const controller = [], imgFigures = [];
     imageDatas.forEach((item, index) => {
-      if(!this.state.imgArrangeArr[index]){
+      if (!this.state.imgArrangeArr[index]) {
         this.state.imgArrangeArr[index] = {
-          pos:{
+          pos: {
             left: 0,
             top: 0
           },
-          rotate : 0,
+          rotate: 0,
           isInverse: false,
           isCenter: false
         }
       }
-      imgFigures.push(<SingleFigure 
-        key={index} 
-        data={item} 
-        ref={'imgFigure' + index} 
-        arrange={this.state.imgArrangeArr[index]}
-        inverse={this.inverse(index)}
-        center={this.center(index)} />)
+      imgFigures.push(<SingleFigure key={index} data={item} ref={'imgFigure' + index} arrange={this.state.imgArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)} />)
+
+      controller.push(<Controller arrange={this.state.imgArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)}/>);
     });
     return (
       <section className='wrap' ref='wrap'>
@@ -241,6 +263,7 @@ class AppComponent extends React.Component {
           {imgFigures}
         </section>
         <nav className='controllArea'>
+          {controller}
         </nav>
       </section>
     );
